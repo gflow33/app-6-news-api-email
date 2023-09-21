e83792d20aae729f73bc57be9a4740e1e3f9b258
Jibril Gupa <gupajibril@gmail.com>
2023-09-21 14:52:37 +0100

    Add links, specify language and other changes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 from send_email import send_email
 
+
+topic = "tesla"
 api_key = "8f59d8ae7cd7496898ab680297fa6f0e"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-21&sortBy=publishedAt&apiKey=8f59d8ae7cd7496898ab680297fa6f0e"
+url = "https://newsapi.org/v2/everything?q=tesla&from=2023-08-21&sortBy=publishedAt&apiKey=8f59d8ae7cd7496898ab680297fa6f0e&language=en"
 
 # Make request
 request = requests.get(url)
@@ -13,8 +15,11 @@ content = request.json()
 
 body = ""
 # Access the articles titles and description
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][:20]:
+    body = "Subject: Today's news" \
+            + "\n" + body + article["title"] + "\n" \
+            + article["description"] \
+            + "\n" + article["url"] + 2*"\n"
 
 
 body = body.encode('utf-8')
